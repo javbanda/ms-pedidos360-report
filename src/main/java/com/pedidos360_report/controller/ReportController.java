6e52f5f1790c8ac01c239546ec6f89b108d54a3b
javbanda <jav.banda@duocuc.cl>
2026-09-12 00:46:20 -0300

    Se actualiza el ReportController con end points y estados activos

--- src/main/java/com/pedidos360_report/controller/ReportController.java
+++ src/main/java/com/pedidos360_report/controller/ReportController.java
@@ -1,15 +1,30 @@
 package com.pedidos360_report.controller;
 
+import com.pedidos360_report.dto.EstadoActivoDTO;
+import com.pedidos360_report.service.ReportService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/report")
 public class ReportController {
 
+    private final ReportService reportService;
+
+    public ReportController(ReportService reportService) {
+        this.reportService = reportService;
+    }
+
     @GetMapping("/kpis")
     public String obtenerKpis() {
         return "Microservicio de reportes funcionando correctamente";
+    }
+
+    @GetMapping("/estados-activos")
+    public List<EstadoActivoDTO> obtenerEstadosActivos() {
+        return reportService.obtenerEstadosActivos();
     }
 }
